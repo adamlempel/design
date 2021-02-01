563811506dc9328cd312f7f9fbeb5dc223e9d953
adamlempel <74027201+adamlempel@users.noreply.github.com>
2021-02-01 12:24:22 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,9 @@ const AboutPage = () => (
     <Section
       title="Adam Lempel is a UX Designer based in New York City"
       text="He is always looking for a better way of doing things. He conceives of new approaches to UX / UI design based on user research and then tests and refines. Adam designs and reiterates apps, websites, and VR / AR interfaces and also designs music for advertisements and sonic branding"
-    ></Section>
+    ><a href="https://docs.google.com/document/d/1xmUiIaBLZrxTfJGy-yPF6XOFA7_SQ9T7JDEZ302fr74/edit">
+        Link to my resume
+      </a></Section>
     {/* <div className="CV">
       <img src="http://box2364.temp.domains/~adamlemp/wp-content/uploads/2020/12/AdamLempel_CV-1.svg"></img>
     </div> */}
